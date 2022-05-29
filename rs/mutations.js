bd850f58b1e567dd5677a67952af18c2f7150cd7
leikareipa <drilledicecream@gmail.com>
2022-05-30 02:51:16 +0300

    Add a basic file for defining mutations

---
+++ rs/mutations.js
@@ -0,0 +1,17 @@
+/*
+ * 2022 Tarpeeksi Hyvae Soft
+ *
+ * Software: rs
+ * 
+ */
+
+export default {
+    "Disable car speed limiter": {
+        author: "Tarpeeksi Hyvae Soft",
+        payload: ["byteset rallye.exe 97482 24"],
+    },
+    "Small cars": {
+        author: "Tarpeeksi Hyvae Soft",
+        payload: ["byteset rallye.exe 1330 0"],
+    },
+}
