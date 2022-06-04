a22194a47c5bde7aa3ebf573e5951d59dec0f10b
leikareipa <drilledicecream@gmail.com>
2022-06-04 06:52:20 +0300

    Always use '$' as the mutation id separator in concatenated id strings
    Rather than using '$' in URL params and ',' in local storage as we were
    doing before this commit.
    
    Note that this commit will invalidate the user's local storage of the
    ids of active mutations, meaning that their latest selection of
    mutations will be wiped clean the next time they load up the app with
    this commit in it and without a URL parameter specifying alternate
    mutation ids. This is due to the app no longer recognizing ',' as an id
    separator and so attempting to match the entire string of local storage
    ids to a single mutation id, which won't be found and so the selection
    is wiped clean.

--- src/rs.js
+++ src/rs.js
@@ -13,6 +13,13 @@ console.assert = function(condition, errorMessage = "") {
     }
 }
 
+// The symbol that separates two mutation ids in a concatenated string of mutation ids;
+// e.g. "abcd!efgh!ijkl" if the separator is '!'. Will be used e.g. when concatenating
+// mutation ids for a URL parameter. NOTE: Changing this value will invalidate existing
+// URL links to the app containing mutation ids as a URL parameter, as well as the
+// user's persistent localStorage of the ids of active mutations.
+const mutationIdSeparator = "$";
+
 const controlPanelEl = document.querySelector("#control-panel");
 const controlPanelCloserEl = document.querySelector("#control-panel-closer");
 const aboutEl = controlPanelEl.querySelector("#about");
@@ -73,22 +80,33 @@ for (const [title, mutation] of Object.entries(mutations).sort()) {
     }
 
     // Effect the user's desired selection of mutations, either via a URL parameter or from
-    // persistent local storage, if either one is available.
+    // persistent local storage, if provided.
     {
-        const allMutationEls = Array.from(mutationContainerEl.querySelectorAll("input[type='checkbox']"));
-        const urlParamData = (new URLSearchParams(window.location.search).get("mutations") || "").trim();
-        const persistentData = (localStorage.getItem("rs:mutation-selection") || "").trim();
+        const selectedMutations = (()=>{
+            const allMutationEls = Array.from(mutationContainerEl.querySelectorAll("input[type='checkbox']"));
+            const urlParamData = (new URLSearchParams(window.location.search).get("mutations") || "").trim();
+            const persistentData = (localStorage.getItem("rs:mutation-selection") || "").trim();
 
-        if (urlParamData.length) {
-            const mutationIds = urlParamData.split("$");
-            allMutationEls.filter(el=>mutationIds.includes(el.dataset.mutationId)).forEach(el=>{el.checked = true; el.onchange()});
+            if (urlParamData.length) {
+                const mutationIds = urlParamData.split(mutationIdSeparator);
+                return allMutationEls.filter(el=>mutationIds.includes(el.dataset.mutationId));
+            }
+            else if (persistentData.length) {
+                const persistedIds = persistentData.split(mutationIdSeparator)
+                return allMutationEls.filter(el=>persistedIds.includes(el.dataset.mutationId));
+            }
+            else {
+                return [];
+            }
+        })();
+
+        if (selectedMutations?.length) {
+            // Apply each mutation. This'll automatically load Rally-Sport in its <iframe>
+            // with the given mutations in effect.
+            selectedMutations.forEach(el=>{el.checked = true; el.onchange()});
         }
-        else if (persistentData.length) {
-            const persistedIds = persistentData.split(",")
-            allMutationEls.filter(el=>persistedIds.includes(el.dataset.mutationId)).forEach(el=>{el.checked = true; el.onchange()});
-        }
-        // Otherwise, load the default, unmodified game.
         else {
+            // Load the default, unmodified game.
             frameEl.src = get_rs_dosbox_url();
         }
     }
@@ -98,14 +116,14 @@ for (const [title, mutation] of Object.entries(mutations).sort()) {
 function on_mutation_selection_changed() {
     const selectedMutations = Array.from(mutationContainerEl.querySelectorAll("input[type='checkbox']:checked"));
     const selectedIdsList = selectedMutations.map(el=>el.dataset.mutationId);
-    const selectedIdsString = selectedIdsList.join(",");
+    const selectedIdsString = selectedIdsList.join(mutationIdSeparator);
     const mutationRunCommands = selectedMutations.map(m=>m.value).join(",");
 
     debounced_update_rs_iframe_src(get_rs_dosbox_url(mutationRunCommands));
     update_mutation_selection_count_label(selectedMutations.length);
     
     localStorage[selectedIdsString.length? "setItem" : "removeItem"]("rs:mutation-selection", selectedIdsString);
-    window.history.replaceState(null, null, (selectedIdsList.length? `?mutations=${selectedIdsList.join("$")}` : "/"));
+    window.history.replaceState(null, null, (selectedIdsList.length? `?mutations=${selectedIdsList.join(mutationIdSeparator)}` : "/"));
 }
 
 // Returns a URL to a ths-web-dosbox (cf. github.com/leikareipa/ths-web-dosbox)
